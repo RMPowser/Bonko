fe4cc47ce4e63385db999149d7a94db0c9949800
Ryan Powser <rpryanpow@gmail.com>
2025-09-09 23:42:24 -0700

    Added Tileset class and Tilemap class

---
+++ Graphics/Tilemap.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Graphics
+{
+	public class Tilemap
+	{
+		private readonly Tileset Tileset;
+		private readonly int[] Tiles;
+		public int Rows { get; }
+		public int Columns { get; }
+		public int Count { get; }
+		public Vector2 Scale { get; set; }
+		public float TileWidth => Tileset.TileWidth * Scale.X;
+		public float TileHeight => Tileset.TileHeight * Scale.Y;
+
+
+		public Tilemap(Tileset tileset, int columns, int rows)
+		{
+			Tileset = tileset;
+			Rows = rows;
+			Columns = columns;
+			Count = Columns * Rows;
+			Scale = Vector2.One;
+			Tiles = new int[Count];
+		}
+
+		public void SetTile(int index, int tilesetID)
+		{
+			Tiles[index] = tilesetID;
+		}
+
+		public void SetTile(int column, int row, int tilesetID)
+		{
+			int index = row * Columns + column;
+			SetTile(index, tilesetID);
+		}
+
+		public TextureRegion GetTile(int index)
+		{
+			return Tileset.GetTile(Tiles[index]);
+		}
+
+		public TextureRegion GetTile(int column, int row)
+		{
+			int index = row * Columns + column;
+			return GetTile(index);
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			for (int i = 0; i < Count; i++)
+			{
+				int tileSetIndex = Tiles[i];
+				TextureRegion tile = Tileset.GetTile(tileSetIndex);
+
+				int x = i % Columns;
+				int y = i / Columns;
+
+				Vector2 position = new(x * TileWidth, y * TileHeight);
+				tile.Draw(spriteBatch, position, Color.White, 0.0f, Vector2.Zero, Scale, SpriteEffects.None, 1.0f);
+			}
+		}
+	}
+}
---
+++ Graphics/Tileset.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Graphics
+{
+	public class Tileset
+	{
+		private readonly TextureRegion[] Tiles;
+		public int TileWidth { get; }
+		public int TileHeight { get; }
+		public int Columns { get; }
+		public int Rows { get; }
+		public int Count { get; }
+
+
+		public Tileset(TextureRegion textureRegion, int tileWidth, int tileHeight)
+		{
+			TileWidth = tileWidth;
+			TileHeight = tileHeight;
+			Columns = textureRegion.Width / tileWidth;
+			Rows = textureRegion.Height / tileHeight;
+			Count = Columns * Rows;
+
+			Tiles = new TextureRegion[Count];
+
+			for (int i = 0; i < Count; i++)
+			{
+				int x = i % Columns * tileWidth;
+				int y = i / Columns * tileHeight;
+				Tiles[i] = new TextureRegion(textureRegion.Texture, textureRegion.SourceRectangle.X + x, textureRegion.SourceRectangle.Y + y, tileWidth, tileHeight);
+			}
+		}
+
+		public TextureRegion GetTile(int index) 
+		{
+			return Tiles[index]; 
+		}
+
+		public TextureRegion GetTile(int column, int row)
+		{
+			int index = row * Columns + column;
+			return GetTile(index);
+		}
+	}
+}
